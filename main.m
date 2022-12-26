193e761979af5c7843b23ba250fb91a8c2e4244c
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-12-26 01:56:43 -0600

    2022-12-26_01:57 --> Perceptron: Clasificador de 4 grupos

--- main.m
+++ main.m
@@ -1,66 +1,75 @@
 % Ian Mu;oz Nu;ez - Perceptron
 
-% Implementar la funcion logica: A(B+C)
-% A B C F
-% 0 0 0 0
-% 0 0 1 0
-% 0 1 0 0
-% 0 1 1 0
-% 1 0 0 0
-% 1 0 1 1
-% 1 1 0 1
-% 1 1 1 1
+% Diseñe una red neuronal unicapa, con función tipo escalón, con dos
+% entradas y dos salidas que sea capaz de clasificar los siguientes diez
+% puntos en el plano, los cuales pertenecen a cuatro clases
+% Grupo 1: (0.1, 1.2), (0.7, 1.8), (0.8, 1.6)
+% Grupo 2: (0.8, 0.6), (1.0, 0.8)
+% Grupo 3: (0.3, 0.5), (0.0, 0.2), (-0.3, 0.8)
+% Grupo 4: (-0.5, -1.5), (-1.5, -1.3)
 
 close all
 clear
 clc
 
-iter = 100;
-xl = -0.2;
-xu = 1.2;
-n = 100;
+iter = 200;
+xl = -1.6;
+xu = 2;
 
-x = [0 0 0 0 1 1 1 1;
-    0 0 1 1 0 0 1 1;
-    0 1 0 1 0 1 0 1];
-b = [1 1 1 1 1 1 1 1];
-d = [0 0 0 0 0 1 1 1];
+x = [0.1 0.7 0.8 0.8 1.0 0.3 0.0 -0.3 -0.5 -1.5];
+y = [1.2 1.8 1.6 0.6 0.8 0.5 0.2 0.8 -1.5 -1.3];
+
+x = [x; y];
+b = [1 1 1 1 1 1 1 1 1 1];
+d1 = [0 0 0 0 0 1 1 1 1 1];
+d2 = [0 0 0 1 1 0 0 0 1 1];
 
 P = [x; b];
 
 w = rand(size(P, 1), 1);
-eta = 0.2;
+eta = 0.1;
 
-figure(1) %%%% Figura 1: Compuerta and
+figure(1)
 hold on
 grid on
 
-[w, e, y] = perceptron(P, w, d, eta, iter);
+[w1, e1, y1] = perceptron(P, w, d1, eta, iter);
+[w2, e2, y2] = perceptron(P, w, d2, eta, iter);
 
-xLim = linspace(xl, xu, n);
-yLim = linspace(xl, xu, n);
-[X, Y] = meshgrid(xLim, yLim);
+t = xl:0.01:xu;
 
-m = -((w(1)*X)/w(3)) - ((w(2)*Y)/w(3));
-b = -w(4)/w(3);
-Z = m + b;
+m1 = -w1(1)/w1(2);
+b1 = -w1(3)/w1(2);
+f1 = m1*t + b1;
+
+m2 = -w2(1)/w2(2);
+b2 = -w2(3)/w2(2);
+f2 = m2*t + b2;
 
 for i=1:size(P, 2)
-    n(i) = w' * P(:, i);
+    n1(i) = w1' * P(:, i);
+    n2(i) = w2' * P(:, i);
 
-    if n(i) >= 0
-        plot3(x(1, i), x(2, i), x(3, i), 'y*', 'LineWidth', 4, 'MarkerSize', 10)
+    if n1(i) >= 0 && n2(i) >= 0
+        plot(x(1, i), x(2, i), 'rx', 'LineWidth', 2, 'MarkerSize', 7)
+
+    elseif n1(i) >= 0 && n2(i) < 0
+        plot(x(1, i), x(2, i), 'bx', 'LineWidth', 2, 'MarkerSize', 7)
+
+    elseif n1(i) < 0 && n2(i) >= 0
+        plot(x(1, i), x(2, i), 'gx', 'LineWidth', 2, 'MarkerSize', 7)
+
     else
-        plot3(x(1, i), x(2, i), x(3, i), 'g*', 'LineWidth', 4, 'MarkerSize', 10)
+        plot(x(1, i), x(2, i), 'yx', 'LineWidth', 2, 'MarkerSize', 7)
     end
 end
-surf(X, Y, Z)
+plot(t, f1, 'c-', 'LineWidth', 2)
+plot(t, f2, 'm-', 'LineWidth', 2)
 
-title("A(B+C)", 'FontSize', 20)
-xlabel('A', 'FontSize', 15)
-ylabel('B', 'FontSize', 15)
-zlabel('C', 'FontSize', 15)
+title("Clasificacion de 4 grupos", 'FontSize', 20)
+xlabel('x', 'FontSize', 15)
+ylabel('y', 'FontSize', 15)
 
-axis([xl xu xl xu xl xu])
 axis equal
+axis([xl xu xl xu])
 
--- main.py
+++ main.py
@@ -1,66 +1,70 @@
 # Ian Mu;oz Nu;ez - Perceptron
 
-# Implementar la funcion logica: A(B+C)
-# A B C F
-# 0 0 0 0
-# 0 0 1 0
-# 0 1 0 0
-# 0 1 1 0
-# 1 0 0 0
-# 1 0 1 1
-# 1 1 0 1
-# 1 1 1 1
+# Diseñe una red neuronal unicapa, con función tipo escalón, con dos
+# entradas y dos salidas que sea capaz de clasificar los siguientes diez
+# puntos en el plano, los cuales pertenecen a cuatro clases
+# Grupo 1: (0.1, 1.2), (0.7, 1.8), (0.8, 1.6)
+# Grupo 2: (0.8, 0.6), (1.0, 0.8)
+# Grupo 3: (0.3, 0.5), (0.0, 0.2), (-0.3, 0.8)
+# Grupo 4: (-0.5, -1.5), (-1.5, -1.3)
 
 import matplotlib.pyplot as plt
 import numpy as np
 from perceptron import Perceptron as perceptron
 
-iter = 100
-xl = -0.2
-xu = 1.2
-n = 100
+iter = 200
+xl = -1.6
+xu = 2
 
-x = [[0, 0, 0, 0, 1, 1, 1, 1],
-     [0, 0, 1, 1, 0, 0, 1, 1],
-     [0, 1, 0, 1, 0, 1, 0, 1]]
-b = [1, 1, 1, 1, 1, 1, 1, 1]
-d = [0, 0, 0, 0, 0, 1, 1, 1]
+x = [0.1, 0.7, 0.8, 0.8, 1.0, 0.3, 0.0, -0.3, -0.5, -1.5]
+y = [1.2, 1.8, 1.6, 0.6, 0.8, 0.5, 0.2, 0.8, -1.5, -1.3]
+
+x = np.vstack((x, y))
+b = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+d1 = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+d2 = [0, 0, 0, 1, 1, 0, 0, 0, 1, 1]
 
 P = np.vstack((x, b))
 w = np.random.rand(P.shape[0], 1)
-eta = 0.2
+eta = 0.1
 
 plt.figure(1)
-ax = plt.axes(projection='3d')
+plt.grid()
 
-w, e, y = perceptron(P, w, d, eta, iter)
+w1, e1, y1 = perceptron(P, w, d1, eta, iter)
+w2, e2, y2 = perceptron(P, w, d2, eta, iter)
 
-xLim = np.linspace(xl, xu, n)
-yLim = np.linspace(xl, xu, n)
-X, Y = np.meshgrid(xLim, yLim)
+t = np.arange(xl, xu, 0.01)
 
-m = -((w[0]*X)/w[2]) - ((w[1]*Y)/w[2])
-b = -w[3]/w[2]
-Z = m + b
+m1 = -w1[0]/w1[1]
+b1 = -w1[2]/w1[1]
+f1 = m1*t + b1
+
+m2 = -w2[0]/w2[1]
+b2 = -w2[2]/w2[1]
+f2 = m2*t + b2
 
 for i in range(0, P.shape[1]):
-    n = np.dot(w.T, P[:, i])
+    n1 = np.dot(w1.T, P[:, i])
+    n2 = np.dot(w2.T, P[:, i])
 
-    if n >= 0:
-        ax.plot3D(x[0][i], x[1][i], x[2][i], 'yo', linewidth=4, markersize=10)
+    if n1 >= 0 and n2 >= 0:
+        plt.plot(x[0, i], x[1, i], 'rx', linewidth=2, markersize=7)
+    elif n1 >= 0 and n2 < 0:
+        plt.plot(x[0, i], x[1, i], 'bx', linewidth=2, markersize=7)
+    elif n1 < 0 and n2 >= 0:
+        plt.plot(x[0, i], x[1, i], 'gx', linewidth=2, markersize=7)
     else:
-        ax.plot3D(x[0][i], x[1][i], x[2][i], 'go', linewidth=4, markersize=10)
-ax.plot_surface(X, Y, Z, cmap='viridis')
+        plt.plot(x[0, i], x[1, i], 'yx', linewidth=2, markersize=7)
+plt.plot(t, f1, 'c-', linewidth=2)
+plt.plot(t, f2, 'm-', linewidth=2)
 
-ax.set_title("A(B+C)", fontsize=20)
+plt.title("Clasificacion de 4 grupos", fontsize=20)
+plt.xlabel('x', fontsize=15)
+plt.ylabel('y', fontsize=15)
 
-ax.set_xlabel('A')
-ax.set_ylabel('B')
-ax.set_zlabel('C')
-
-ax.set_xlim(xmin=xl, xmax=xu)
-ax.set_ylim(ymin=xl, ymax=xu)
-ax.set_zlim(zmin=xl, zmax=xu)
+plt.axis('equal')
+plt.axis([xl, xu, xl, xu])
 
 plt.show()
 
